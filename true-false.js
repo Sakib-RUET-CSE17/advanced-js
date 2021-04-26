59c44ba0248c590a67879da973fcfb373eceb799
Sakib Hasan <hsakib8685@gmail.com>
2021-04-27 02:54:04 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,22 @@
+//0
+//''
+//undefined
+//null
+//NaN
+//------------------------------
+//all are falsy
+
+//========================
+//truthy
+//
+
+
+// const name = ''
+let name = []
+console.log(name)
+if (name) {
+    console.log('true')
+} else {
+    console.log('false')
+
+}
