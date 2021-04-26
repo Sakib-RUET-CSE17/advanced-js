8722af29e47a81ca893952bb131d67fe4938973f
Sakib Hasan <hsakib8685@gmail.com>
2021-04-27 03:20:42 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,10 @@
+// let pakhi
+// function add(num1, num2) {
+//     console.log(num1 + num2)
+
+// }
+function add(num1, num2 = 10) {
+    console.log(num2)
+}
+const result = add(13)
+console.log(result)
